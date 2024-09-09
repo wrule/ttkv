70de5af299ec47aefed7314e8a47f338e57703fb
jimao <shihao.gu@perfma.com>
2024-09-09 18:09:37 +0800

    set get

--- index.ts
+++ index.ts
@@ -7,10 +7,31 @@ class TTKV {
   public constructor(file: string) {
     if (!fs.existsSync(file)) makeDB(file);
     this.db = sqlite3(file);
-    db.pragma('journal_mode = WAL');
+    this.db.pragma('journal_mode = WAL');
   }
 
   private db: Database;
+
+  public async set(key: string, value: string) {
+    const insertStmt = this.db.prepare(`INSERT OR IGNORE INTO ttkv (createTime, updateTime, key, value) VALUES (?, ?, ?, ?)`);
+    const updateStmt = this.db.prepare(`UPDATE ttkv SET value = ?, updateTime = ? WHERE key = ?`);
+    const time = Date.now();
+    await insertStmt.run(time, time, key, value);
+    await updateStmt.run(value, time, key);
+  }
+
+  public async get(key: string) {
+    const selectStmt = this.db.prepare(`SELECT value FROM ttkv WHERE key = ?`);
+    const item = await selectStmt.get(key) as { value: string } | undefined;
+    return item?.value;
+  }
 }
 
-const db = sqlite3('3.db');
+async function main() {
+  const tt = new TTKV('3.db');
+  await tt.set('鸡毛', '爱你12343');
+  console.log(await tt.get('鸡毛'));
+  
+}
+
+main();
